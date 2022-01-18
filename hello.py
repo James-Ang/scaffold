6eeb398a06ec6c096dcf4c3461f112d1d9e2e7f7
james <james83ang@hotmail.com>
2022-01-18 20:06:20 +0800

    add text

--- hello.py
+++ hello.py
@@ -3,3 +3,9 @@ print("The quick brown fox jumps over the laze dog.")
 l = [1, 2, 3, 4, 5, 6]
 
 print(l)
+
+print(
+    "Hello there. \
+    my name is James \
+    . Would you like to come here?"
+)
